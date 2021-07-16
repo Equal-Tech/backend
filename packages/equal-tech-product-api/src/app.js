08daf930d793df01dc196945306f65de0510a8db
George MacKay-Shore <george.mackay-shore@and.digital>
2021-07-17 00:50:35 +0100

    chore: split app and server

--- packages/equal-tech-product-api/src/app.js
+++ packages/equal-tech-product-api/src/app.js
@@ -2,10 +2,7 @@ const express = require('express');
 const apiRoutes = require('./routes');
 
 const app = express();
-const port = 3000;
 
 app.use('/api', apiRoutes);
 
-app.listen(port, () => {
-  console.log(`Product API listening on http://localhost:${port}`);
-});
+module.exports = app;

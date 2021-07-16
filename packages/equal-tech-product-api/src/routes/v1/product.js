f39de24d19582b0ad601ff44fdbd60228b5b76d6
George MacKay-Shore <george.mackay-shore@and.digital>
2021-07-16 23:33:57 +0100

    feat: return one product on /product endpoint based on id

--- packages/equal-tech-product-api/src/routes/v1/product.js
+++ packages/equal-tech-product-api/src/routes/v1/product.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const getOneProduct = require('../../services/getOneProduct');
 
 const router = express.Router();
 
-router.get('/:productId', (req, res) => res.send(req.params.productId));
+router.get('/:productId', (req, res) => {
+  const { productId } = req.params;
+  const product = getOneProduct(Number.parseInt(productId));
+  if (!product) {
+    return res.status(404).send({ message: 'Not found' });
+  }
+
+  res.send(product);
+});
 
 module.exports = router;
---
+++ packages/equal-tech-product-api/src/services/getOneProduct.js
@@ -0,0 +1,6 @@
+const products = require('../../data/products.json');
+
+const getOneProduct = (productId) =>
+  products.find((product) => product.id === productId);
+
+module.exports = getOneProduct;

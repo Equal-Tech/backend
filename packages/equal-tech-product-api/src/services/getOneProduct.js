1eeb8fb7d12b2ce7edc101a73a38c86721a61eb3
George MacKay-Shore <george.mackay-shore@and.digital>
2021-08-25 16:30:45 +0100

    feat: update tests for `getOneProduct`

--- packages/equal-tech-product-api/src/services/getOneProduct.js
+++ packages/equal-tech-product-api/src/services/getOneProduct.js
@@ -1,6 +1,6 @@
 const products = require('../../data/products.json');
 
 const getOneProduct = (productId) =>
-  products.find((product) => product.id === productId);
+  products.find((product) => product.id === productId) || {};
 
 module.exports = getOneProduct;
